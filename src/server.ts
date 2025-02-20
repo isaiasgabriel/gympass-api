0cc95dc6539653854470de834b4e854db119b6a9
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2025-02-20 16:12:24 -0300

    ⚙️ ESLint configuration
    npm i -D eslint @rocketseat/eslint-config
    .eslintrc.json:
    
    {
      "extends": [
        "@rocketseat/eslint-config/node"
      ]
    }
    
    package.json:
    
    "lint": "eslint src/ --fix"
    
    CTRL + SHIFT + P > settings.json:
    
      "editor.codeActionsOnSave": {
        "source.fixAll.eslint": "always",
      },

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
-import { app } from "./app";
+import { app } from './app'
 
-app.listen({
-  host: '0.0.0.0', // makes it accessible on the front end
-  port:3333
-}).then(()=>{
-  console.log("HTTP SERVER RUNNING")
-})
+app
+  .listen({
+    host: '0.0.0.0', // makes it accessible on the front end
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP SERVER RUNNING')
+  })
