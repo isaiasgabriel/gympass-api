3e082c3ac80bcdb81b3f625df284bd4b705fc5e7
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2025-02-20 23:57:22 -0300

    🔺 Prisma configuration
    npm i prisma -D
    npm i @prisma/client
    
    npx prisma init > prisma/schema.prisma
    @@map("users") > table name displayed in the database
    
    After creating your tables:
    npx prisma generate > create TS types for better development

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+// Checking the types created from prisma (npx prisma generate)
+prisma.user.create({
+  data: {
+    name: 'test',
+    email: 'test',
+  },
+})
