60b3c2cdb5e20c6ba7f3355558088cc108295f44
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2025-02-27 10:32:34 -0300

    ✨ Vitest UI
    Install package: npm i -D @vitest/ui
    Add package.json script: "test:ui": "vitest --ui"

--- src/use-cases/register-use-case.ts
+++ src/use-cases/register-use-case.ts
@@ -1,6 +1,6 @@
 import { UsersRepositoryInterface } from '@/repositories/user-repository-interface'
 import { hash } from 'bcryptjs'
-import { UserAlreadyExistsError } from './errors/user-already-exists'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 import { User } from '@prisma/client'
 
 interface RegisterUseCaseRequest {
